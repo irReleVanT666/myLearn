c89b8a7e6094c4a798a4cef0f385084dcb91a9de
Student 2 <st2@example.com>
2020-12-07 20:27:24 +0400

    test

---
+++ hw_3_2.py
@@ -0,0 +1,10 @@
+print ('4etnie ili ne4etnie?')
+varAnsw = input()
+i = 0
+if varAnsw == '4etnie':
+	for i in range(1,100,2):
+	print ('4etnie:',i, sep ',')
+else:
+	for i in range(0,100,2);
+	print ('Ne4etnie :',i, sep ',')
+	
---
+++ lesson2.py
@@ -0,0 +1,10 @@
+print ('4etnie ili ne4etnie?')
+varAnsw = input()
+i = 0
+if varAnsw == 'ne':
+	for i in range(1,20,2):
+		print (i, end=',')
+if varAnsw == '4et':
+	for i in range(0,20,2):
+		print (i, end=',')
+	
