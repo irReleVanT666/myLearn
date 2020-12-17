412d0f305d55b3b8940e6ae90740e3b15177586f
Student 2 <st2@example.com>
2020-12-17 18:50:01 +0400

    class3

---
+++ class_3.py
@@ -0,0 +1,16 @@
+my_str = 'Hello world'
+# print("Hello", "World", sep='\n', end='')
+
+# new_str = my_str + ' + New substr'
+
+# new_str= my_str[6:11]
+
+# new_str= my_str[6:]
+
+# new_str= my_str[::-1]
+#
+# print (new_str)
+
+a = 'hello'
+a += ' world'
+print(a)
